450db48fdcb78cf908e96407c451e0ef1d1080e1
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-09-01 19:39:36 +0300

    added content via api

--- js/script.js
+++ js/script.js
@@ -5,14 +5,52 @@ const loadCategory = async () => {
     displayCategory(data.data);
 }
 
+//fetching all content data
+const loadAllContent = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    const data = await res.json();
+    console.log(data.data);
+    displayAllContent(data.data);
+}
+
+
+
+//display content
+const displayAllContent = (content) => {
+    const allContentContainer = document.getElementById('all-container');
+
+    content.forEach(content => {
+        // const categoryTitle = content.title;
+        // console.log(categoryTitle);
+        const div = document.createElement('div');
+        div.classList = `p-3 w-full`
+        div.innerHTML = `<img class="my-4 h-56 w-full rounded-lg" src="${content.thumbnail}" alt="" />
+        
+        <div class="flex gap-5 ml-1">
+        
+            <img class="w-11 h-11 rounded-full" src="${content?.authors[0]?.profile_picture}" alt=""/>
+
+            <div>
+                <p class="font-bold text-base">${content?.title}</p>
+                <p class="text-sm text-gray-500 my-2 font-normal">${content?.authors[0]?.profile_name}</p>
+                <p class="text-gray-500 text-sm font-normal">${content?.others?.views} views</p>
+            </div>
+        </div>`
+        
+        allContentContainer.appendChild(div);
+     });
+     
+
+}
+loadAllContent();
+
+//display category
 const displayCategory = (items) => {
     // console.log(items);
     const categoryDiv = document.getElementById('category-div');
     const categoryUl = categoryDiv.childNodes[1];
    
        
-    
-    
     items.forEach(item => {
        const categoryName = item.category;
         const li = document.createElement('li');
